d86a46f52bf561f57326603371ff7ac6d4eabaaf
Kunal Sinha <mogh@example.com>
2016-02-01 11:40:55 -0800

    added to the model

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ActiveRecord::Base
+	belongs_to :category
+	validates_presence_of :title, :content
+	scope :alphabetical, order('name')
+    scope :active, where('active = ?', true)
+end
